b7b49fddd44b64b603b28d1de1b0f22f95357a6d
Deepak Patel <deepak@divyaltech.com>
2023-03-06 15:04:59 +0530

    app built

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['links.papareact.com', 'fakestoreapi.com'],
+  }
 }
 
 module.exports = nextConfig
