ec2cb2c911f98671b7605033e3777ee91e7c8e2e
Deepak Patel <deepak@divyaltech.com>
2023-03-16 17:28:56 +0530

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
             {/* top nav */}
             <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
                 <div className="mt-2 mx-4 flex items-center flex-grow sm:flex-grow-0">
-                    <Image
+                    <img
                         onClick={() => router.push('/')}
                         src="https://links.papareact.com/f90"
                         width={150}
